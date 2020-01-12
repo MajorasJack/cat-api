d43d42d0e37fca22c5f36664d0628fa3c31edf7f
Jack Evans <jack.evans@boohoo.com>
2020-01-12 13:40:50 +0000

    Searrch updates, new endpoint and updates tests

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,12 @@ Route::get('/', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/search', 'OmdbController@search')->name('omdb.search');
+Route::get('/search', 'SearchController@index')->name('movie.search');
+
+Route::get('/search/id', 'MovieSearchController@index')->name('movie.search.id');
 
 Route::get('/movies', 'MovieController@index')->name('movies.index');
+Route::get('/movies/{id}', 'MovieController@show')->name('movies.search');
 Route::post('/movies/create', 'MovieController@store')->name('movies.create');
 Route::patch('/movies/update', 'MovieController@update')->name('movies.update');
 Route::delete('/movies/destroy', 'MovieController@destroy')->name('movies.destroy');
