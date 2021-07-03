0baa896c4c301dce65bc0c95fd25fd888ff36032
Jack Evans <jack@lavatech.ltd.uk>
2021-07-03 16:45:01 +0100

    Updating prophecy trait

--- tests/Support/UsesProphecyMocks.php
+++ tests/Support/UsesProphecyMocks.php
@@ -1,86 +1,56 @@
 <?php
 
-namespace Tests\Support;
+namespace Tests\Support\Traits;
 
-use Closure;
+use Prophecy\Exception\Prediction\AggregateException;
 use Prophecy\Prophecy\ObjectProphecy;
 use Prophecy\Prophet;
-use Prophecy\Argument;
 
 trait UsesProphecyMocks
 {
     /**
+     * An instance of Prophet used to generate mocks
+     *
      * @var Prophet
      */
-    protected Prophet $prophet;
-
+    protected $prophet;
     /**
-     * @var Argument
-     */
-    protected Argument $argument;
-
-    /**
-     * @return void
-     */
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->prophet = new Prophet;
-        $this->argument = new Argument;
-    }
-
-    /**
+     * Camel cased as it extends a phpunit method.
+     *
+     * Check that any predictions/expectationsdeclared on mocks made are
+     * fulfilled, and that no extra calls were made that were not expected.
+     *
+     * `checkPredictions()` does this and behaves as an assertion, but throws
+     * errors rather than passing or failing a test. For this reason, convert
+     * any exceptions into a pass/fail message.
+     *
      * @return void
      */
     public function tearDown(): void
     {
-        if ($this->prophet->getProphecies()) {
-            $this->prophet->checkPredictions();
-            // The above is an assertion (it will fail if expected calls aren't made/
-            // unexpected calls are made) but phpunit doesn't recognise these. If no
-            // exception is thrown here, assert true to stop phpunit reporting the
-            // test as risky if no other assertions are made.
-            $this->assertTrue(true);
+        if ($this->prophet && $this->prophet->getProphecies()) {
+            try {
+                $this->prophet->checkPredictions();
+                $this->assertTrue(true);
+            } catch (AggregateException $e) {
+                $this->fail($e->getMessage());
+            }
         }
         parent::tearDown();
     }
 
     /**
-     * @param $abstract
-     * @param Closure|null $mock
+     * Create and return a mocked version of the given class, on which
+     * expectations can be declared.
+     *
+     * @param string $class The class to be mocked.
      * @return ObjectProphecy
      */
-    public function mock($abstract, ?Closure $mock = null): ObjectProphecy
+    public function mock(string $class): ObjectProphecy
     {
-        return $this->prophet->prophesize($abstract);
-    }
-
-    /**
-     * @param string $facade
-     * @param ObjectProphecy $mock
-     * @param $resolver
-     * @return void
-     */
-    public function facadeShouldReturn(string $facade, ObjectProphecy $mock, $resolver)
-    {
-        $manager = $this->mock(get_class($facade::getFacadeRoot()));
-        switch (gettype($resolver)) {
-            case 'string' :
-                $manager->$resolver()->shouldBeCalled()->willReturn($mock->reveal());
-                break;
-            case 'array':
-                foreach ($resolver as $callback) {
-                    $callback($manager)->shouldBeCalled()->willReturn($mock->reveal());
-                }
-                break;
-            case 'object':
-                $resolver($manager)->shouldBeCalled()->willReturn($mock->reveal());
-                break;
-            default:
-                throw new BadArgumentException(
-                    'Invalid resolver in UsesProphecyMocks::facadeShouldReturn'
-                );
+        if (!$this->prophet) {
+            $this->prophet = new Prophet();
         }
-        $facade::swap($manager->reveal());
+        return $this->prophet->prophesize($class);
     }
 }
